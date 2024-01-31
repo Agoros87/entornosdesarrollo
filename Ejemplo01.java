f22844b76920ca432d50f1e2497a2d4da4740a77
smrv21-11 <smrv21-11@c16x15.iescierva.net>
2024-01-31 19:13:43 +0100

    ED47 Añadir bucle for al archivo Ejemplo01

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -3,5 +3,11 @@ public class Ejemplo01{
 		System.out.println("Hola mundo");
 		String nombre = "Antonio";
 		System.out.println("Mi nombre es --> " + nombre);
+		for(int i = 1 ; i <= 10 ; i++){
+			System.out.println("Vuelta numero: " + i + 1);
+			for(int i = 1 ; i <= 10 ; i++){
+				System.out.println("Vuelta numero: " +i);
+		}
+		}
 	}
 }
