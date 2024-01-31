5940b4d0d7d4d54c1fe6b4c7df9fb7ea95f3d89e
smrv21-11 <smrv21-11@c16x15.iescierva.net>
2024-01-31 19:23:21 +0100

    Resolver conflictos

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -3,14 +3,9 @@ public class Ejemplo01{
 		System.out.println("Hola mundo");
 		String nombre = "Antonio";
 		System.out.println("Mi nombre es --> " + nombre);
-
 		System.out.println("La suma de 2 y 3 es" + (2+3);
-
-		
 			for(int i = 1 ; i <= 10 ; i++){
 				System.out.println("Vuelta numero: " +i);
-		
 		}
-
 	}
 }
