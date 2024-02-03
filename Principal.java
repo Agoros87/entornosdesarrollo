e5bae0a631180dbc6d761bf308d3502cafcbdf6e
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-02-03 18:43:19 +0100

    Principal He implementado un metodo para ver la fecha y la hora

--- Principal.java
+++ Principal.java
@@ -1,5 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Principal{
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		dameFechaYHora();
+	}
+
+	public static void dameFechaYHora(){
+
+	LocalDateTime fechaYHora = LocalDateTime.now();
+
+	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+	String fechaYHoraFormateada = fechaYHora.format(formato);
+
+	System.out.println("La fecha y la hora actual es: " + fechaYHoraFormateada);
 	}
 }
+
